a73c054f7170a75cfca8f6196ad28a492e95ff24
Michał <skolakmichal1@gmail.com>
2024-02-29 21:01:27 +0100

    feat: create desktop view of about section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,3 +1,4 @@
+import { Corner } from '@/assets/shapes/corner';
 import { InclinedLine } from '@/assets/shapes/inclined-line';
 import { Semicircle } from '@/assets/shapes/semicircle';
 import { SectionHeading } from '@/components/section-heading';
@@ -23,33 +24,34 @@ const data = [
 
 export const About = () => {
   return (
-    <section className="container relative space-y-10">
-      <Semicircle className="absolute left-1/2 top-1/2 size-64 -translate-x-1/2 -translate-y-1/2" />
+    <section className="container relative mb-10 space-y-10 md:mb-32 md:space-y-32">
+      <Semicircle className="absolute left-1/2 top-1/2 size-80 -translate-x-1/2 -translate-y-2/3 md:size-[600px] md:translate-y-[-55%]" />
+      <Corner className="absolute bottom-0 right-6 hidden md:block" />
       <SectionHeading>about</SectionHeading>
-      <div className="space-y-5">
-        <div className="space-y-5">
-          <h3 className="xs:text-5xl font-serif text-4xl uppercase md:text-7xl lg:text-9xl">
+      <div className="space-y-5 md:space-y-10 lg:space-y-20">
+        <div className="space-y-5 md:flex md:justify-between md:gap-10 md:space-y-0">
+          <h3 className="xs:text-5xl font-serif text-4xl uppercase md:text-7xl lg:text-8xl">
             mission
           </h3>
-          <p>
+          <p className="md:max-w-xl">
             Our core mission is to maintain the utmost standards of quality and
             transparency in our services. We are dedicated to perpetual growth
             and refinement, with the aim of consistently surpassing industry
-            standards.{' '}
+            standards.
           </p>
         </div>
         <span className="bg-primary block h-0.5 w-72" />
-        <p>
+        <p className="md:max-w-xl">
           Additionally, our steadfast commitment to collaborating with NGOs
           underscores our unfaltering dedication to effective positive change in
           both society and the global community.
         </p>
       </div>
-      <div className="space-y-5">
-        <h3 className="xs:text-5xl after:bg-primary relative font-serif text-4xl uppercase after:absolute after:bottom-1.5 after:ml-4 after:size-4 after:rounded-full md:text-7xl lg:text-9xl">
+      <div className="space-y-5 md:flex md:justify-between md:gap-10 md:space-y-0">
+        <h3 className="xs:text-5xl after:bg-primary relative h-fit font-serif text-4xl uppercase after:absolute after:bottom-1.5 after:ml-4 after:size-4 after:rounded-full md:text-7xl md:after:bottom-2.5 md:after:size-6 lg:text-8xl lg:after:bottom-3 lg:after:ml-6 lg:after:size-8">
           vision
         </h3>
-        <p>
+        <p className="md:max-w-xl">
           Our vision encompasses setting and reshaping industry standards while
           fostering innovation and collaboration with like-minded organizations.
           We are firmly dedicated to the pursuit of excellence, aspiring to be a
@@ -57,17 +59,19 @@ export const About = () => {
           dynamic world of design and product strategy.
         </p>
       </div>
-      <div className="flex flex-col space-y-5">
-        <div className="flex items-center">
-          <h3 className="xs:text-5xl font-serif text-4xl uppercase md:text-7xl lg:text-9xl">
+      <div className="flex flex-col space-y-5 md:flex-row md:items-start md:justify-between">
+        <div className="flex items-center md:order-2">
+          <h3 className="xs:text-5xl font-serif text-4xl uppercase md:text-7xl lg:text-8xl">
             values
           </h3>
           <InclinedLine className="xs:size-10 ml-4 size-8 md:size-14 lg:ml-14 lg:mr-0 lg:size-20" />
         </div>
-        <div className="space-y-8">
+        <div className="space-y-8 md:order-1 md:space-y-14">
           {data.map(({ title, content }) => (
-            <div key={title} className="space-y-2.5">
-              <h4 className="text-3xl font-extralight uppercase">{title}</h4>
+            <div key={title} className="space-y-2.5 md:space-y-5">
+              <h4 className="max-w-md text-3xl font-extralight uppercase md:text-4xl lg:text-5xl">
+                {title}
+              </h4>
               <p>{content}</p>
             </div>
           ))}
